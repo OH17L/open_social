33406d4905a4fcbe0745cf9a446f8dc4c4405a4f
Oleksandr Horbatiuk <oleksandr.horbatiuk@lemberg.co.uk>
2017-12-05 17:20:07 +0200

    PAC-382 by chmez: Create a list of users which were commented post.

--- modules/custom/activity_basics/src/Plugin/ActivityContext/CommentOwnerActivityContext.php
+++ modules/custom/activity_basics/src/Plugin/ActivityContext/CommentOwnerActivityContext.php
@@ -2,7 +2,12 @@
 
 namespace Drupal\activity_basics\Plugin\ActivityContext;
 
+use Drupal\activity_creator\ActivityFactory;
 use Drupal\activity_creator\Plugin\ActivityContextBase;
+use Drupal\Core\Database\Connection;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Entity\Query\Sql\QueryFactory;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Provides a 'CommentOwnerActivityContext' activity context.
@@ -15,19 +20,67 @@ use Drupal\activity_creator\Plugin\ActivityContextBase;
 class CommentOwnerActivityContext extends ActivityContextBase {
 
   /**
+   * The database connection.
+   *
+   * @var \Drupal\Core\Database\Connection
+   */
+  protected $database;
+
+  /**
    * {@inheritdoc}
    */
-  public function getRecipients(array $data, $last_uid, $limit) {
-    $recipients = [];
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, QueryFactory $entity_query, EntityTypeManagerInterface $entity_type_manager, Connection $database) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $entity_query, $entity_type_manager);
 
-    return $recipients;
+    $this->database = $database;
   }
 
   /**
    * {@inheritdoc}
    */
-  public function isValidEntity($entity) {
-    return TRUE;
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('entity.query.sql'),
+      $container->get('entity_type.manager'),
+      $container->get('database')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getRecipients(array $data, $last_uid, $limit) {
+    $recipients = [];
+
+    if (isset($data['related_object']) && !empty($data['related_object'])) {
+      $related_entity = ActivityFactory::getActivityRelatedEntity($data);
+
+      /** @var \Drupal\social_post\Entity\PostInterface $post */
+      $post = $this->entityTypeManager->getStorage('post')
+        ->load($related_entity['target_id']);
+
+      $uids = $this->database->select('comment_field_data', 'c')
+        ->distinct()
+        ->fields('c', ['uid'])
+        ->condition('entity_type', 'post')
+        ->condition('entity_id', $related_entity['target_id'])
+        ->condition('uid', $post->getOwnerId(), '!=')
+        ->condition('uid', $data['actor'], '!=')
+        ->execute()
+        ->fetchCol();
+
+      foreach ($uids as $uid) {
+        $recipients[] = [
+          'target_type' => 'user',
+          'target_id' => $uid,
+        ];
+      }
+    }
+
+    return $recipients;
   }
 
 }
